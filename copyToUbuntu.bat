cf620cc2c28ae392f9317d2620d104c93abc1de9
Alf <megre_fr@mail.ru>
2026-08-26 15:22:15 +0300

    Exclude local deployment scripts

--- copyToUbuntu.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d C:\Cabinet
-
-tar --exclude=CabinetReact/.git ^
-    --exclude=CabinetReact/node_modules ^
-    --exclude=CabinetReact/dist ^
-    -czf - CabinetReact | ssh user@192.168.7.233 "rm -rf /home/user/Programs/CabinetReact && cd /home/user/Programs && tar -xzf -"
-
-pause
